4f116fa84e56437fc15ea9d8a5c6bcbea7f36561
Mohammad Kotb <mohammad.i.kotb@gmail.com>
2011-12-23 16:09:46 +0200

    first commit, created rails project

---
+++ doit/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doit::Application.config.secret_token = '4c36fac742003843f9d644e65c71499ee4ea345079da6463006842d34c07935d5c3b8aa79e262c1d3b169d5f16dc069d3abe98185f20c9797c7bc06858f00af2'
